3db27f5868ed0e5490ef045b08627a1924d47fb5
erickr007 <eric@erics-mbp>
2017-12-04 19:21:13 -0800

    Initial Commit

---
+++ ParkingPal/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ParkingPal
+//
+//  Created by Eric Robertson on 12/4/17.
+//  Copyright © 2017 Eric Robertson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ParkingPal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ParkingPal
+//
+//  Created by Eric Robertson on 12/4/17.
+//  Copyright © 2017 Eric Robertson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
